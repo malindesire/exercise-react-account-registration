ca99f6ca403817ef79aa28209e6e89560dd2ed39
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-27 13:55:13 +0200

    Add functionality to get and display values from form

--- account-registration/src/components/Form/index.tsx
+++ account-registration/src/components/Form/index.tsx
@@ -1,8 +1,27 @@
+import { useState } from 'react';
 import { Button } from '../Button';
 import { Input } from '../Input';
 import styles from './style.module.css';
+import { type RegistrationData } from '../../utilities/types';
 
 export const Form = () => {
+	const [data, setData] = useState<RegistrationData>({
+		name: '',
+		username: '',
+		email: '',
+		password: '',
+	});
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setData((prev) => ({ ...prev, [name]: value }));
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log(data);
+	};
+
 	return (
 		<form className={styles.form}>
 			<Input label="Fullname" type="text" name="name" />
