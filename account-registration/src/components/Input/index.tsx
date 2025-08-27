86c869a65a32cf3dc74490be1df33c325c624f13
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-27 13:52:37 +0200

    Add onChange to Input component

--- account-registration/src/components/Input/index.tsx
+++ account-registration/src/components/Input/index.tsx
@@ -4,13 +4,19 @@ type InputProps = {
 	name: string;
 	label: string;
 	type: 'text' | 'email' | 'password';
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const Input = ({ name, label, type }: InputProps) => {
+export const Input = ({ name, label, type, onChange }: InputProps) => {
 	return (
 		<label className={styles.label}>
 			{label}
-			<input type={type} name={name} required />
+			<input
+				type={type}
+				name={name}
+				onChange={onChange}
+				autoComplete={name}
+			/>
 		</label>
 	);
 };
